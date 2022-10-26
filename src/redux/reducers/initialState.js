698fe83b6b0f3ea33201d3eb179b74cd5af5f211
Mugerwa Fred <reifred33@gmail.com>
2022-10-27 00:12:16 +0300

    Redux react setup

---
+++ src/redux/reducers/initialState.js
@@ -0,0 +1,11 @@
+export const initialLoginState = {
+    loggedIn: false,
+    loggingIn: false,
+    user: {},
+};
+
+export const initialSignUpState = {
+    registered: false,
+    registering: false,
+    user: {},
+};
