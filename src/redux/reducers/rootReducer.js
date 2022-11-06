fc7669ca134d2ef8361a597a7e95004b64dc47d7
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-11-06 22:06:44 +0300

    Update rootReducer.js

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,5 +1,9 @@
 import { combineReducers } from "redux";
+import loginReducer from "./loginReducer";
+import registerReducer from "./registerReducer"
+
 
 export default combineReducers({
-    // List of reducers
+    loginReducer,
+    registerReducer
 });
