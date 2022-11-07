19e399e26b0d46aa86e6d48828e29415bd1664c3
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-11-07 20:59:42 +0300

    Update loginReducer.js

--- src/redux/reducers/loginReducer.js
+++ src/redux/reducers/loginReducer.js
@@ -1,9 +1,33 @@
 import { initialLoginState } from "./initialState";
-
+import axios from "axios";
+import qs from 'qs'
 function loginReducer(state = initialLoginState, action) {
     switch (action.type) {
 
         case 'LOGIN_REQUEST':
+            // const {username,password} = action.payload
+            // const params = new URLSearchParams({
+            //     username,
+            //     password
+            // })
+
+            const data = {
+                method:'POST',
+                headers:{
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body:action.payload
+
+            }
+            try{
+                fetch('https://musutropicals.herokuapp.com/musubackend/api/login',data).then(
+                response=>console.log(response)
+            )
+            }
+            catch(e){
+                console.log('Error ',e)
+            }
 
             if (action.payload.email === 'root') {
                 //test authentication
